323974a22c847f1ab19f6979874651df6eb3f40c
用户名 <317991755@qq.com>
2020-05-09 12:15:12 +0800

    项目初始化-autoprefixer的配置和配置postcss-pxtorem

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,23 @@
 /**
  * PostCSS 的配置文件
 */
+
+// PostCSS 配置文件需要导出一个对象
 module.exports = {
+  // 配置要使用的相关插件
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
+    // 自动添加浏览器厂商声明前缀，用来兼容不同的浏览器
+    // VueCLI 已经在内部配置了 autoprefixer
+    // autoprefixer: {
+    //   // browsers 用来配置要兼容的系统（浏览器）环境
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
+
+    // 把 px 转化成 rem
     'postcss-pxtorem': {
+      // 750 宽的设计稿：750 / 10 = 75
+      // 375 宽的设计稿：375 / 10 = 37.5
+      // Vant 组件库是基于逻辑像素 375 写的
       rootValue: 37.5,
       propList: ['*']
     }
