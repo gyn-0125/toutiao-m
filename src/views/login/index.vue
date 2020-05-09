eb4f5b2ff8c5309644f181580a748235205456dd
用户名 <317991755@qq.com>
2020-05-09 16:44:39 +0800

    登录注册-页面布局（样式）

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="login-container">
+    <!-- 导航栏 -->
     <van-nav-bar
-      title="登录"
+      class="app-nav-bar"
+      title="注册 / 登录"
       left-arrow
       @click-left="$router.back()"
     />
@@ -10,21 +12,33 @@
     <van-cell-group>
       <van-field
         v-model="user.mobile"
-        left-icon="smile-o"
+        icon-prefix="iconfont icon"
+        left-icon="shouji"
         placeholder="请输入手机号"
       />
       <van-field
         v-model="user.code"
         clearable
-        left-icon="music-o"
+        icon-prefix="iconfont icon"
+        left-icon="yanzhengma"
         placeholder="请输入验证码"
       >
         <template #button>
-          <van-button size="mini" round>发送验证码</van-button>
+          <van-button
+            class="send-btn"
+            size="mini"
+            round
+          >发送验证码</van-button>
         </template>
       </van-field>
     </van-cell-group>
-    <van-button type="info" block>登录</van-button>
+    <div class="login-btn-wrap">
+      <van-button
+        type="info"
+        block
+        class="login-btn"
+      >登录</van-button>
+    </div>
   </div>
 </template>
 
@@ -49,4 +63,26 @@ export default {
 }
 </script>
 
-<style lang='less' scoped></style>
+<style lang='less' scoped>
+.login-container {
+  .send-btn {
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text {
+      font-size: 11px;
+      color: #666;
+    }
+  }
+  .login-btn-wrap {
+    padding: 26px 16px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+      .van-button__text {
+        font-size: 15px;
+      }
+    }
+  }
+}
+</style>
