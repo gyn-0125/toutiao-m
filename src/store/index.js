ee64b33b76c0797823f9c6e5ee3a0a10df4a5ac1
用户名 <317991755@qq.com>
2020-05-10 14:53:35 +0800

    登录注册-存储用户token处理完成

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: JSON.parse(window.localStorage.getItem('user')) // 当前登录用户的登录状态（token数据）
   },
   mutations: {
+    setUser (state, data) {
+      state.user = data
+
+      window.localStorage.setItem('user', JSON.stringify(state.user))
+    }
   },
   actions: {
   },
