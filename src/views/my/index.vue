cb5fcde2f839e98217c5ca09f24bd3ae8b2ef522
用户名 <317991755@qq.com>
2020-05-10 15:54:52 +0800

    我的-页面布局（头部用户信息）

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="my-container">我的页面</div>
+  <div class="my-container">
+    <div class="user-info">
+      <van-cell-group>
+        <van-cell
+          title="单元格"
+          value="内容"
+          center
+        >
+          <van-image
+            slot="icon"
+            width="50"
+            height="50"
+            round
+            fit="cover"
+            src="https://img.yzcdn.cn/vant/cat.jpeg"
+          />
+          <div slot="title">昵称</div>
+          <van-button
+            size="small"
+            round
+          >编辑资料</van-button>
+        </van-cell>
+      </van-cell-group>
+    </div>
+  </div>
 </template>
 
 <script>
