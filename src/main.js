8ff9e7ee8c0f551ff053602bc832dd2ab19d9a00
用户名 <317991755@qq.com>
2020-05-10 09:41:45 +0800

    登录注册-调整全局样式资源加载的位置

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 加载全局样式
-import './styles/index.less'
-import './styles/icon.less'
-
 // 加载Vant组件库
 import Vant from 'vant'
 // 加载Vant组件库样式
@@ -14,6 +10,10 @@ import 'vant/lib/index.css'
 
 // 自动设置 REM 基准值（html 标签字体大小）
 import 'amfe-flexible'
+
+// 加载全局样式(最好放到最后，方便覆盖第三方样式)
+import './styles/index.less'
+import './styles/icon.less'
 
 // 全局注册 Vant 中的组件
 Vue.use(Vant)
