c2ad7633f0b2a8ceefaf79d870a8bdb9edf48825
用户名 <317991755@qq.com>
2020-05-12 10:00:50 +0800

    我的-页面布局（样式）-头部背景

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,53 +1,55 @@
 <template>
   <div class="my-container">
-    <div class="user-info">
-      <van-cell-group>
-        <van-cell
-          title="单元格"
-          value="内容"
-          center
-        >
-          <van-image
-            slot="icon"
-            width="50"
-            height="50"
-            round
-            fit="cover"
-            src="https://img.yzcdn.cn/vant/cat.jpeg"
-          />
-          <div slot="title">昵称</div>
-          <van-button
-            size="small"
-            round
-          >编辑资料</van-button>
-        </van-cell>
-        <van-grid>
-          <van-grid-item>
-            <div slot="text">
-              <div class="span">123</div>
-              <div class="text">头条</div>
-            </div>
-          </van-grid-item>
-          <van-grid-item>
-            <div slot="text">
-              <div class="span">123</div>
-              <div class="text">关注</div>
-            </div>
-          </van-grid-item>
-          <van-grid-item>
-            <div slot="text">
-              <div class="span">123</div>
-              <div class="text">粉丝</div>
-            </div>
-          </van-grid-item>
-          <van-grid-item>
-            <div slot="text">
-              <div class="span">123</div>
-              <div class="text">获赞</div>
-            </div>
-          </van-grid-item>
-        </van-grid>
-        <van-grid :column-num="2">
+    <van-cell-group class="my-info">
+      <van-cell
+        class="base-info"
+        title="单元格"
+        value="内容"
+        center
+        :border="false"
+      >
+        <van-image
+          slot="icon"
+          width="50"
+          height="50"
+          round
+          fit="cover"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+        <div slot="title">昵称</div>
+        <van-button
+          size="small"
+          round
+        >编辑资料</van-button>
+      </van-cell>
+      <van-grid :border="false">
+        <van-grid-item>
+          <div slot="text">
+            <div class="span">123</div>
+            <div class="text">头条</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <div class="span">123</div>
+            <div class="text">关注</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <div class="span">123</div>
+            <div class="text">粉丝</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <div class="span">123</div>
+            <div class="text">获赞</div>
+          </div>
+        </van-grid-item>
+      </van-grid>
+    </van-cell-group>
+    <van-grid :column-num="2">
           <van-grid-item
             icon-prefix="iconfont icon"
             icon="shoucang"
@@ -58,13 +60,10 @@
             icon="lishi"
             text="历史"
           />
-        </van-grid>
-
-        <van-cell title="消息通知" is-link to="/" />
-        <van-cell title="小智同学" is-link to="/" />
-        <van-cell title="退出登录" />
-      </van-cell-group>
-    </div>
+      </van-grid>
+      <van-cell title="消息通知" is-link to="/" />
+      <van-cell title="小智同学" is-link to="/" />
+      <van-cell title="退出登录" />
   </div>
 </template>
 
@@ -84,4 +83,17 @@ export default {
 }
 </script>
 
-<style lang='less' scoped></style>
+<style lang='less' scoped>
+.my-container {
+  .my-info {
+    background: url('./banner.png') no-repeat;
+    background-size: cover;
+    .base-info {
+      background-color: unset;
+    }
+    /deep/ .van-grid-item__content {
+      background-color: unset;
+    }
+  }
+}
+</style>
