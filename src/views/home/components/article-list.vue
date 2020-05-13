806b766442dcbb9908965e5330ad1ae47ae84e65
用户名 <317991755@qq.com>
2020-05-13 21:31:32 +0800

    首页文章列表-展示文章列表数据

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -7,10 +7,10 @@
       @load="onLoad"
     >
       <van-cell
-        v-for="item in list"
-        :key="item"
-        :title="item"
-      ></van-cell>
+        v-for="(article, index) in articles"
+        :key="index"
+        :title="article.title"
+      />
     </van-list>
   </div>
 </template>
@@ -29,9 +29,10 @@ export default {
   components: {},
   data () {
     return {
-      list: [], // 数据列表
+      articles: [], // 数据列表
       loading: false, // 控制加载中的 loading 状态
-      finished: false // 控制加载结束的状态，当加载结束时，不再触发加载更多
+      finished: false, // 控制加载结束的状态，当加载结束时，不再触发加载更多
+      timestamp: null // 获取下一页数据的时间戳
     }
   },
   computed: {},
@@ -40,15 +41,30 @@ export default {
   mounted () {},
   methods: {
     async onLoad () {
-      console.log('onload')
+      // console.log('onload')
       // 1.请求获取数据
       const { data } = await getArticles({
         channel_id: this.channel.id,
-        timestamp: 1589337751688,
+        timestamp: this.timestamp || Date.now(), // 时间戳
         with_top: 1 //  是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
       })
 
-      console.log(data)
+      // console.log(data)
+      // 2.把数据缩放到 list 数组中
+      const { results } = data.data
+      this.articles.push(...results)
+
+      // 3.设置本次加载状态结束，它才可以判断是否需要加载下一次，否则就会永远的停在这里
+      this.loading = false
+
+      // 4.数据全部加载完成
+      if (results.length) {
+        // 更新获取下一页数据的页码
+        this.timestamp = data.data.pre_timestamp
+      } else {
+        // 没有数据了，把加载状态设置结束，不再触发加载更多
+        this.finished = true
+      }
     }
   }
 }
