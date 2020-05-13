23746dfff1e49cc780251b1da14ed75392a8a521
用户名 <317991755@qq.com>
2020-05-13 22:28:54 +0800

    首页文章列表-样式-处理三个封面图

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -6,12 +6,14 @@
         v-if="article.cover.type === 3"
         class="cover-wrap"
       >
-        <div class="cover-wrap-item">
+        <div
+          class="cover-wrap-item"
+          v-for="(img, index) in article.cover.images"
+          :key="index"
+        >
           <van-image
-            class="right-cover"
+            class="cover-image"
             fit="cover"
-            v-for="(img,index) in article.cover.images"
-            :key="index"
             :src="img"
           />
         </div>
@@ -25,11 +27,10 @@
     <!-- 默认插槽的名字叫 default，可以省略 -->
     <van-image
       v-if="article.cover.type === 1"
-      width="116"
-      height="73"
+      class="right-cover"
       fit="cover"
       :src="article.cover.images[0]"
-    ></van-image>
+    />
   </van-cell>
 </template>
 
@@ -69,6 +70,28 @@ export default {
   .right-cover {
     width: 116px;
     height: 73px;
+  }
+  .cover-wrap {
+    padding: 15px 0;
+    display: flex;
+    .cover-wrap-item {
+      flex: 1;
+      height: 73px;
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-image {
+        width: 100%;
+        height: 73px;
+      }
+    }
+  }
+  .label-wrap {
+    font-size: 11px;
+    color: #b4b4b4;
+  }
+  .label-wrap span {
+    margin-right: 12px;
   }
 }
 </style>
