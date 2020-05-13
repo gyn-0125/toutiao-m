29de8a0c6b451658bfd1eb0701ef18a303affe1f
用户名 <317991755@qq.com>
2020-05-13 20:42:06 +0800

    首页文章列表-页面布局-导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="home-contianer">首页</div>
+  <div class="home-contianer">
+    <van-nav-bar class="app-nav-bar">
+      <van-button
+        slot="title"
+        class="search-btn"
+        icon="search"
+        type="info"
+        round
+      >搜索</van-button>
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
@@ -18,4 +28,21 @@ export default {
 }
 </script>
 
-<style lang='less' scoped></style>
+<style lang='less' scoped>
+.home-contianer {
+  /deep/ .van-nav-bar__title {
+    max-width: none;
+  }
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background-color: #5babfb;
+    .van-icon {
+      font-size: 16px;
+    }
+    .van-button__text {
+      font-size: 14px;
+    }
+  }
+}
+</style>
